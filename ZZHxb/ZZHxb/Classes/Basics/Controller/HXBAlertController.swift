d94e97761608a277bf1065001651c9b253989499
李向洲 <lixiangzhouno1@163.com>
2018-01-17 17:08:10 +0800

    HXBAlertController

---
+++ ZZHxb/ZZHxb/Classes/Basics/Controller/HXBAlertController.swift
@@ -0,0 +1,66 @@
+//
+//  HXBAlertController.swift
+//  ZZHxb
+//
+//  Created by lxz on 2018/1/17.
+//Copyright © 2018年 lixiangzhou. All rights reserved.
+//
+
+import UIKit
+
+class HXBAlertController: UIViewController {
+
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setUI()
+    }
+
+    // MARK: - Public Property
+    
+    // MARK: - Private Property
+    
+}
+
+// MARK: - UI
+extension HXBAlertController {
+    fileprivate func setUI() {
+        
+    }
+}
+
+// MARK: - Action
+extension HXBAlertController {
+    
+}
+
+// MARK: - Network
+extension HXBAlertController {
+    
+}
+
+// MARK: - Delegate Internal
+
+// MARK: -
+
+// MARK: - Delegate External
+
+// MARK: -
+
+// MARK: - Helper
+extension HXBAlertController {
+    
+}
+
+// MARK: - Other
+extension HXBAlertController {
+    
+}
+
+// MARK: - Public
+extension HXBAlertController {
+    
+}
+
